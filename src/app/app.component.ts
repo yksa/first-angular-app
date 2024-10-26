648f21c2267f6584ab9a10ffe013e4f7441628ab
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-27 01:01:55 +0700

    Defining Component Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,6 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   title = 'first-angular-app';
+
+  users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,14 +8,12 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex()]);
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser.avatar}`;
-  // }
-
-  onSelectUser() {
-    this.selectedUser.set(DUMMY_USERS[randomIndex()]);
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onSelectUser() {}
 }
