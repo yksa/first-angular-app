c02a5b1fa087f5808217a3ca124def2b691954b9
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-27 01:53:49 +0700

    Add Task Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,8 +15,13 @@ export class AppComponent {
   title = 'first-angular-app';
 
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
