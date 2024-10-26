08ac81d2d61570bef3c16a930afea9eaabd41ec7
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-27 02:07:40 +0700

    Accepting Objects as inputs & adding appropriate typings

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -17,17 +17,22 @@ import {
 })
 export class UserComponent {
   // Zone
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
   // Signal
