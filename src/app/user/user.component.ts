f33a7492c05f5de6cde0063b18ccea99ae360497
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-27 16:31:36 +0700

    Content Projection with ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,7 @@ import {
   computed,
 } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 // interface User {
 //   id: string;
@@ -18,7 +19,7 @@ import { type User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
