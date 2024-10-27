28ced69027fd9367f468ddd9af4d12336cb16e67
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-27 16:47:08 +0700

    Getting Started With Services

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,24 @@
+import { dummyTasks } from '../dummy-tasks';
+import { newTask } from './task/task.model';
+
+class TasksService {
+  private tasks = dummyTasks;
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(task: newTask, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: task.title,
+      summary: task.summary,
+      dueDate: task.dueDate,
+    });
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
